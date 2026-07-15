181ea7a3ba47e8fb56257fa3d382cb1e9db17713
Talita Narciso <talitanarciso33@gmail.com>
2026-07-14 21:06:10 -0300

    .

---
+++ atividade01.php
@@ -0,0 +1,41 @@
+<?php
+/*Uma fábrica deseja verificar se um funcionário atingiu sua meta semanal.
+● armazene o nome do funcionário;
+● armazene a quantidade produzida de segunda a sexta-feira;
+● calcule o total produzido durante a semana;
+● calcule a média diária de produção;
+● a meta semanal, que será de 500 unidades;
+● a diferença entre o total produzido e a meta.
+Condição:
+● Se o total produzido for maior ou igual a 500, exiba que a meta foi alcançada e calcule
+um bônus de R$ 0,20 por unidade produzida acima da meta.
+● Caso contrário, exiba que a meta não foi alcançada e informe quantas unidades
+faltaram.
+Exiba todos os resultados calculados.
+*/
+
+//Entradas - variáveis
+
+
+// Processamento
+
+
+
+
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atividade 1</title>
+</head>
+<body>
+    
+</body>
+</html>
